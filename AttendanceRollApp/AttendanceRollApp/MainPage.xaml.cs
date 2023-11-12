862f9dc84e96bfe32ba97ffc7ff28c2bdc2a1299
RHV044 <s_rhv@hotmail.com>
2023-11-11 21:03:44 -0300

    Template Project init

---
+++ AttendanceRollApp/AttendanceRollApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AttendanceRollApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

db34d7c7bbac1c9c663be07bc3c858d90c290ccf
RHV044 <s_rhv@hotmail.com>
2023-11-12 10:51:49 -0300

    Razor lib added

---
+++ AttendanceRollApp/AttendanceRollAppRazorLib/Areas/MyFeature/Pages/Page1.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AttendanceRollAppRazorLib.MyFeature.Pages
+{
+    public class Page1Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
